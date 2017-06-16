40436f131c68251cae54283d50af39f891ecaf3b
John Exby <exby@ucar.edu>
2017-06-16 14:47:41 -0600

    corrections to mpi mca params file, spot market use for AWS demo

--- 3.7.1/demos/aws/spinup-new-ec2.sh
+++ 3.7.1/demos/aws/spinup-new-ec2.sh
@@ -1,3 +1,7 @@
+#
+# instead of paying a whopping $1.59 per hour for a c4.8large virtual machine, let's save money by using Spot instances!
+# aws ec2 describe-spot-price-history --instance-types c4.8xlarge --availability-zone us-west-2b --start-time 2017-05-30T16:00:00 --end-time 2017-05-30T18:00:00
+#
 docker-machine -D create --driver amazonec2 \
 --amazonec2-access-key $AWS_ACCESS_KEY_ID \
 --amazonec2-secret-key $AWS_SECRET_ACCESS_KEY \
@@ -5,4 +9,7 @@ docker-machine -D create --driver amazonec2 \
 --amazonec2-region us-west-2 \
 --amazonec2-instance-type c4.8xlarge \
 --amazonec2-root-size 60 \
---amazonec2-zone b wrf-LargeEC2
+--amazonec2-request-spot-instance \
+--amazonec2-spot-price 0.625 \
+--amazonec2-zone b \
+wrf-LargeEC2
